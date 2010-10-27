9426929bd3befee1eca4c0be573afebfd8519282
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-27 12:27:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/3d-carousel-menu/trunk@304907 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 3d-carousel-menu-fx.php
+++ 3d-carousel-menu-fx.php
@@ -79,7 +79,7 @@ License: GPL2
 	}
 
 	function carouselmenufx_echo_embed_code($settings_xml_path = '', $div_text = '', $width = 0, $height = 0) {
-		echo carouselmenufx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text, 4 => $width, 6 => $height));
+		echo carouselmenufx_get_embed_code(array(2 => $settings_xml_path, 7 => $div_text, 4 => $width, 6 => $height));
 	}
 
 	function carouselmenufx_load_swfobject_lib() {
